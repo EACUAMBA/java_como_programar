5e2900341f593ea6eee39d65bf380b9682b7142f
EACUAMBA <EdilsonCuamba@gmail.com>
2020-10-03 09:08:39 +0200

    Learned about print in screen in one line using two commands and how to use escape caracteres to organize more the text.

---
+++ chapter-2/2.2-modifie-our-first-java-program/Welcome2.java
@@ -0,0 +1,12 @@
+
+/*
+* Project from: Edilson A. Cuamba
+* Created on 03 - October - 2020
+*/
+public class Welcome2 {
+	public static void main(String... args) {
+		System.out.print("Welcome to ");
+		System.out.println("Java Programming!");
+	}
+}
+
---
+++ chapter-2/2.2-modifie-our-first-java-program/Welcome3.java
@@ -0,0 +1,11 @@
+
+/*
+* Project from: Edilson A. Cuamba
+* Created on 03 - October - 2020
+*/
+public class Welcome3 {
+	public static void main(String... args) {
+		System.out.print("Welcome\nto\nJava\nProgramming!");
+	}
+}
+
