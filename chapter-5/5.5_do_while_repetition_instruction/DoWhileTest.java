725894c3a1fcdb10fbf8eea9f71b0e6979a46c2d
EACUAMBA <edilsoncuamba@gmail.com>
2020-12-29 18:37:33 +0200

    Learned how to use switch with String, char, int, short, byte.

---
+++ chapter-5/5.5_do_while_repetition_instruction/DoWhileTest.java
@@ -0,0 +1,11 @@
+public class DoWhileTest{
+	public static void main(String... args){
+		int counter = 1;
+		
+		do{
+			System.out.printf("%d%n", counter);
+			counter++;
+		}while(counter <=10);
+		
+	}
+}
