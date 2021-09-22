68449d5f9206486a070741a9aff849a9795b2804
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2021-09-22 18:12:26 +0200

    Aprendi a usar a seleção com JComboBox e a seleção usando JList, e aprendi a implementar o JScrollPane para cirar barras de rolagem qundo o conteudo for muito grande.

---
+++ src/chapter_12/_12_12_jlist/JListJFrame.java
@@ -0,0 +1,41 @@
+package chapter_12._12_12_jlist;
+
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import java.awt.*;
+
+public class JListJFrame extends JFrame {
+    private final JList<String> stringJList;
+    private final String[] colorsName;
+    private final Color[] colors;
+
+    public JListJFrame(){
+        super("Usando JList");
+        this.setLayout(new FlowLayout());
+
+        this.colorsName =new String[]{"Black", "White", "Red", "Yellow", "Green", "Azul", "Pink", "Cyan"};
+        this.colors = new Color[]{Color.BLACK, Color.WHITE, Color.RED, Color.YELLOW, Color.GREEN, Color.BLUE, Color.PINK, Color.CYAN};
+
+        this.stringJList = new JList<>(this.colorsName);
+        this.stringJList.setVisibleRowCount(5);
+        this.stringJList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setViewportView(this.stringJList);
+
+        this.add(scrollPane);
+
+        this.stringJList.addListSelectionListener(
+                new ListSelectionListener() {
+                    @Override
+                    public void valueChanged(ListSelectionEvent e) {
+                        int itemSelecionado = JListJFrame.this.stringJList.getSelectedIndex();
+                        JListJFrame.this.getContentPane().setBackground(JListJFrame.this.colors[itemSelecionado]);
+                    }
+                }
+        );
+
+    }
+
+}
