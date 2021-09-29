7907501cedaba33ac729093133c134becfff904c
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2021-09-29 15:34:01 +0200

    Aprende sobre como como tratar o evento da tecla do teclado e o sobre Layouts, Grid, Border, Flow

---
+++ src/chapter_12/_12_17_tratamentos_de_eventos_de_teclado/KeyFrameDemoContext.java
@@ -0,0 +1,12 @@
+package chapter_12._12_17_tratamentos_de_eventos_de_teclado;
+
+import javax.swing.*;
+
+public class KeyFrameDemoContext {
+    public static void main(String[] args) {
+        KeyDemoFrame keyDemoFrame = new KeyDemoFrame();
+        keyDemoFrame.setVisible(true);
+        keyDemoFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        keyDemoFrame.setSize(500, 500);
+    }
+}
