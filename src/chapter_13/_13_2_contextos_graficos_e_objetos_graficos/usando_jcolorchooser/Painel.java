712ed8658c776e8a43a9063132e823e60d0f1f85
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2021-10-04 21:43:59 +0200

    Aprendi a usar a classe Color e a usar JColorChooser para abrir um painel de selecao de cor.

---
+++ src/chapter_13/_13_2_contextos_graficos_e_objetos_graficos/usando_jcolorchooser/Painel.java
@@ -0,0 +1,45 @@
+package chapter_13._13_2_contextos_graficos_e_objetos_graficos.usando_jcolorchooser;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Painel extends JPanel {
+    private final JButton colorChooseButton;
+    private final JColorChooser jColorChooser;
+    private Color color;
+
+    public Painel (){
+        this.colorChooseButton = new JButton("Selecionar cor");
+        this.jColorChooser = new JColorChooser();
+        this.color = Color.WHITE;
+
+        this.setBackground(this.color);
+
+        this.colorChooseButton.addActionListener(
+                new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        Painel.this.color = JColorChooser.showDialog(Painel.this, "Escolha a cor", color);
+
+                        if(color ==null)
+                            color = Color.WHITE;
+
+                        Painel.this.setBackground(color);
+                    }
+                }
+        );
+
+    }
+
+    public static void main(String[] args) {
+        JFrame jf = new JFrame();
+        Painel painel = new Painel();
+        jf.add(painel, BorderLayout.CENTER);
+        jf.add(painel.colorChooseButton, BorderLayout.SOUTH);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.setVisible(true);
+        jf.setSize(500, 500);
+    }
+}
