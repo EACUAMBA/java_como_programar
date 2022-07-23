64b71f9577290e09234b25d12fdab5dd3ef37f19
Edilson A. Cuamba <edilsoncuamba@gmail.com>
2022-07-23 22:39:21 +0200

    Aprendendo a utilizar streams com intStream.

---
+++ src/main/java/com/eacuamba/dev/chapter_17_lambdas_e_fluxos_java_se_8/_17_3_operacoes_intstream/IntStreamOperations.java
@@ -0,0 +1,46 @@
+package com.eacuamba.dev.chapter_17_lambdas_e_fluxos_java_se_8._17_3_operacoes_intstream;
+
+import java.security.SecureRandom;
+import java.util.stream.IntStream;
+
+public class IntStreamOperations {
+    public static void main(String[] args) {
+        int[] values = new int[20];
+        for (int i = 0; i < 20; i++)
+            values[i] = new SecureRandom().nextInt(20) + 1;
+
+        System.out.print("Original Values: ");
+        IntStream.of(values).forEach(value -> System.out.printf("%d ", value));
+        System.out.println();
+
+        System.out.printf("%nCount: %d%n", IntStream.of(values).count());
+        System.out.printf("Min: %d%n", IntStream.of(values).min().getAsInt());
+        System.out.printf("Max: %d%n", IntStream.of(values).max().getAsInt());
+        System.out.printf("Sum: %d%n", IntStream.of(values).sum());
+        System.out.printf("Average (Media): %.2f%n", IntStream.of(values).average().getAsDouble());
+
+        System.out.printf("Sum via reduce method: %d%n", IntStream.of(values).reduce(0, (x, y) -> x + y));
+        System.out.printf("Sum of squares via reduce method: %d%n", IntStream.of(values).reduce(0, (x, y) -> x + y * y));
+        System.out.printf("Product via reduce method: %d%n", IntStream.of(values).reduce(1, (x, y) -> x * y));
+        System.out.printf("Even values displayed in sorted order: ");
+        IntStream.of(values)
+                .filter(value -> value % 2 == 0)
+                .sorted()
+                .forEach((value)-> System.out.printf("%d ", value));
+        System.out.println();
+
+        System.out.print("Odd values multiplied by 10 displayed in sorted order: ");
+        IntStream.of(values)
+                .filter(value -> value % 2 != 0)
+                .map(value -> value * 10)
+                .sorted()
+                .forEach((value)-> System.out.printf("%d ", value));
+        System.out.println();
+
+        System.out.printf("Sum of integer from 1 to 9 (Exclusive): %d%n", IntStream.range(1, 9).sum());
+        System.out.printf("Sum of integer from 1 to 10 (Inclusive): %d%n", IntStream.rangeClosed(1, 10).sum());
+
+
+
+    }
+}
