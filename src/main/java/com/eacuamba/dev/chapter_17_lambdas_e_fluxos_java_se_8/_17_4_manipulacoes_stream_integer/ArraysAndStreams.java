23c29faa2fc6f6de50c40ef8d7608be9fc5b71e5
Edilson A. Cuamba <edilsoncuamba@gmail.com>
2022-07-28 10:00:28 +0200

    Aprendi a criar uma stream com qualquer array e processar esses dados do array.

---
+++ src/main/java/com/eacuamba/dev/chapter_17_lambdas_e_fluxos_java_se_8/_17_4_manipulacoes_stream_integer/ArraysAndStreams.java
@@ -0,0 +1,25 @@
+package com.eacuamba.dev.chapter_17_lambdas_e_fluxos_java_se_8._17_4_manipulacoes_stream_integer;
+
+import java.security.SecureRandom;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ArraysAndStreams {
+    public static void main(String[] args) {
+        Integer[] values = new Integer[20];
+        for(int i = 0; i<20; i++)
+            values[i] = new SecureRandom().nextInt(15);
+
+        System.out.printf("Original values: %s%n", Arrays.asList(values));
+        System.out.printf("Sorted values: %s%n", Arrays.stream(values).sorted().collect(Collectors.toList()));
+
+        List<Integer> greaterThanFour = Arrays.stream(values).filter(value -> value > 4).collect(Collectors.toList());
+        System.out.printf("Values greater than 4: %s%n", greaterThanFour);
+
+        System.out.printf("Sorted values greater than 4: %s%n", Arrays.stream(values).filter(integer -> integer > 4).sorted().collect(Collectors.toList()));
+        System.out.printf("Values greater than 4 (ascending with streams): %s%n", greaterThanFour.stream().sorted().collect(Collectors.toList()));
+
+
+
+    }
+}
