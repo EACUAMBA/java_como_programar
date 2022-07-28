808a90e7a6e8620930a53b9b6ec0c496a3cad0a2
Edilson A. Cuamba <edilsoncuamba@gmail.com>
2022-07-28 13:55:29 +0200

    Aprendi a criar uma stream com qualquer array e processar esses dados do array.

--- src/main/java/com/eacuamba/dev/chapter_17_lambdas_e_fluxos_java_se_8/_17_4_manipulacoes_stream_integer/ArraysAndStreams.java
+++ src/main/java/com/eacuamba/dev/chapter_17_lambdas_e_fluxos_java_se_8/_17_4_manipulacoes_stream_integer/ArraysAndStreams.java
@@ -18,8 +18,5 @@ public class ArraysAndStreams {
 
         System.out.printf("Sorted values greater than 4: %s%n", Arrays.stream(values).filter(integer -> integer > 4).sorted().collect(Collectors.toList()));
         System.out.printf("Values greater than 4 (ascending with streams): %s%n", greaterThanFour.stream().sorted().collect(Collectors.toList()));
-
-
-
     }
 }
