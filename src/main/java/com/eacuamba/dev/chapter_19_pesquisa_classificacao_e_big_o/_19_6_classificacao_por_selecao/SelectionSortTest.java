edf64960213f967b99cf390d631d3927ca515c04
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2023-01-15 16:07:18 +0200

    Commented loop instruction.

--- src/main/java/com/eacuamba/dev/chapter_19_pesquisa_classificacao_e_big_o/_19_6_classificacao_por_selecao/SelectionSortTest.java
+++ src/main/java/com/eacuamba/dev/chapter_19_pesquisa_classificacao_e_big_o/_19_6_classificacao_por_selecao/SelectionSortTest.java
@@ -74,7 +74,7 @@ public class SelectionSortTest {
     }
 
     public static void main(String[] args) {
-        for(int i =0; i < 10000; i++)
+        ///for(int i =0; i < 10000; i++)
             executarTest();
     }
 }
